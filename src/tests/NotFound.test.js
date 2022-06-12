53fa547e57b8f4565bdb8eaf62e639886ec34c99
Brainon Queiroz <brainonqueiroz@gmail.com>
2022-06-12 01:22:56 -0300

    Req 4 OK

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+test('Testa se o texto Page request no found aparece', () => {
+  renderWithRouter(<NotFound />);
+  const titleEl = screen.getByRole('heading',
+    { name: /Page requested not found/i, level: 2 });
+  expect(titleEl).toBeInTheDocument();
+});
+
+test('Testa se aparece uma determinada imagem', () => {
+  const imgSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  renderWithRouter(<NotFound />);
+  const imgEl = screen.getByAltText(
+    'Pikachu crying because the page requested was not found',
+  );
+  expect(imgEl).toBeInTheDocument();
+  expect(imgEl).toHaveAttribute('src', imgSrc);
+});
